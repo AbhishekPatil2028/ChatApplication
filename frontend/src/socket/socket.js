1e301d0e9cc0c5b24a527a5142ac5fb7e9b5f72a
Abhishek Patil <abhiapatil2028@gmail.com>
2026-03-24 12:17:32 +0530

    fix socket connection

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const chatSocket = io(import.meta.env.VITE_SOCKET_URL, {
+const chatSocket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
   transports: ["websocket"],
   withCredentials:true,
