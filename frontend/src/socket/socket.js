7bb3964232a0f0a18e43cfa1976795eefaae3c5e
Abhishek Patil <abhiapatil2028@gmail.com>
2026-03-24 12:51:18 +0530

    fix socket + env

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client";
 
 const chatSocket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
-  transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000
